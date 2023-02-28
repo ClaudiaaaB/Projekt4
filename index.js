e995b8a4b8c42f229ecf5dd36991437d1b474980
Klaudia <banaszek.k@gazeta.pl>
2023-02-28 21:12:48 +0100

    ostatnie poprawy

--- index.js
+++ index.js
@@ -1,61 +1,28 @@
 const formElement = document.querySelector(".form-form");
-// const currencyWantedElement = document.querySelector(".js-currencyWanted");
 const currencyOwnedElement = document.querySelector(".js-currencyOwned");
 const moneyOwnedElement = document.querySelector(".js-moneyOwned");
-const symbolElement = document.querySelector("js-symbol");
+const symbolElement = document.querySelector(".js-symbol");
 const exchangedAmountElement = document.querySelector(".js-exchangedAmount");
 const toShowResult = document.querySelector(".final-result");
 
 const getButton = document.getElementById("get-button");
 getButton.addEventListener("click", () => {
-  console.log("tekst");
-  fetch("https://api.nbp.pl/api/exchangerates/rates/A/EUR/")
+  console.log(currencyOwnedElement.value);
+  fetch(
+    "https://api.nbp.pl/api/exchangerates/rates/A/" +
+      currencyOwnedElement.value +
+      "/"
+  )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      calculateRates(data);
     });
 });
-const getRates = (rates) => {
-  for (const rate in rates) {
-    const opt = document.createElement("option");
-    opt.value = rate;
-    opt.innerText = rate;
-    select.appendChild(opt);
+const calculateRates = (data) => {
+  if (moneyOwnedElement.value < 0) {
+    alert("wartośc powinna być większa od 0");
   }
+  exchangedAmountElement.innerText = (
+    data.rates[0].mid * moneyOwnedElement.value
+  ).toFixed(2);
 };
-
-currencyOwnedElement.addEventListener("input", () => {
-  if (currencyOwnedElement.value === "eur") {
-    currencyWantedElement.value = "pln";
-  } else currencyWantedElement.value = "eur";
-});
-
-currencyOwnedElement.addEventListener("input", () => {
-  if (currencyOwnedElement.value === "usd") {
-    currencyWantedElement.value = "pln";
-  } else currencyWantedElement.value = "usd";
-});
-
-currencyOwnedElement.addEventListener("input", () => {
-  if (currencyOwnedElement.value === "chf") {
-    currencyWantedElement.value = "pln";
-  } else currencyWantedElement.value = "chf";
-});
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let moneyOwned = moneyOwnedElement.value;
-
-  if (currencyOwnedElement.value === "eur") {
-    let exchangedAmount = moneyOwned;
-    exchangedAmountElement.innerText = exchangedAmount.toFixed;
-    symbolElement.innerText = "EU";
-  } else if (currencyOwnedElement.value === "pln") {
-    let exchangedAmountElement = moneyOwned;
-    exchangedAmountElement.innerText = exchangedAmount.toFixed;
-    symbolElement.innerText = "PLN";
-  }
-});
-
-toShowResult.textContent = result;
