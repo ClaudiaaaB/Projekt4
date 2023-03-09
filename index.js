8c7d3b10e61d2556e4188d029b70a0a4a3e68aa3
Klaudia <banaszek.k@gazeta.pl>
2023-03-09 18:18:56 +0100

    alert

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const formElement = document.querySelector(".form-form");
 const currencyOwnedElement = document.querySelector(".js-currencyOwned");
 const moneyOwnedElement = document.querySelector(".js-moneyOwned");
-const symbolElement = document.querySelector(".js-symbol");
 const exchangedAmountElement = document.querySelector(".js-exchangedAmount");
-const toShowResult = document.querySelector(".final-result");
 
 const getButton = document.getElementById("get-button");
 getButton.addEventListener("click", () => {
-  console.log(currencyOwnedElement.value);
+  if (moneyOwnedElement.value < 0 || moneyOwnedElement.value === "") {
+    alert("wartośc powinna być większa od 0");
+    return;
+  }
+
   fetch(
     "https://api.nbp.pl/api/exchangerates/rates/A/" +
       currencyOwnedElement.value +
@@ -18,10 +19,8 @@ getButton.addEventListener("click", () => {
       calculateRates(data);
     });
 });
+
 const calculateRates = (data) => {
-  if (moneyOwnedElement.value < 0) {
-    alert("wartośc powinna być większa od 0");
-  }
   exchangedAmountElement.innerText = (
     data.rates[0].mid * moneyOwnedElement.value
   ).toFixed(2);
